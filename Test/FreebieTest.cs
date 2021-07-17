ac6fdfd1c95109f56d9ac095e55bcd545eb1629c
Kushal Hada <khada@nexient.com>
2021-07-17 09:11:04 -0400

    add a freebie test

---
+++ Test/FreebieTest.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+
+namespace Test
+{
+    public class FreebieTest
+    {
+        [Fact]
+        public void AddTwoNumbers()
+        {
+            // Arrange
+            var oneDollar = 1.0M;
+            var twoDollars = 2.0M;
+            var threeDollars = 3.0M;
+            var expected = threeDollars;
+
+
+            // Act 
+            var actual = oneDollar + twoDollars;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
